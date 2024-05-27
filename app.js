51e18c1fd618b3226dc2ab31f14b6dd43bb40d77
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-05-28 02:46:51 +0500

    complete function

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
 let eidDate = new Date('Jun 16, 2024').getTime();
 let currentDate = new Date();
 
-
 let diff =  eidDate - currentDate;
 
 let day = Math.floor(diff / (1000 *60 * 60 * 24 ));
 let hour = Math.floor(diff / ( 1000 * 60 * 60 ));
 let mints = Math.floor(diff / ( 1000 * 60 ));
 let sec = Math.floor(diff / ( 1000));
+let box = document.querySelector ('.box');
+box.innerHTML = `<h1>Day : ${day} <span>Hour : ${hour}</span></h1><p>Minutes : ${mints}</br> Seconds : ${sec}</p>`;
+if(day = 0 , hour = 0 ,mints = 0 , sec = 0){
+    box.innerHTML = `<h1> EID MUBARAK </h1>`;
 
-let mainbox = document.querySelector ('.mainbox');
-mainbox.innerHTML = `<h1>Day : ${day} </br> Hour : ${hour} </br> Minutes : ${mints} </br> Seconds : ${sec}</h1>`
+}
 
-
-
-
-
-
-
-console.log(day ,hour ,mints);
