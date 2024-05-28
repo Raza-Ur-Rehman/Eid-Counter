8c2a6a70e0c5a6bdf1def92c56c83630ffb2797d
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-05-29 01:19:25 +0500

    interval function update

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 let eidDate = new Date('Jun 16, 2024').getTime();
-let currentDate = new Date();
 
+let countDown = setInterval (() => {
+    
+    let currentDate = new Date().getTime();
 let diff =  eidDate - currentDate;
 
 let day = Math.floor(diff / (1000 *60 * 60 * 24 ));
-let hour = Math.floor(diff / ( 1000 * 60 * 60 ));
-let mints = Math.floor(diff / ( 1000 * 60 ));
-let sec = Math.floor(diff / ( 1000));
+let hour = Math.floor((diff % ( 1000 * 60 * 60 * 24 )) / (1000 * 60 *60));
+let mints = Math.floor((diff % ( 1000 * 60 * 60 )) / (1000 * 60 )) ;
+let sec = Math.floor((diff % ( 1000 * 60)) / (1000));
 let box = document.querySelector ('.box');
 box.innerHTML = `<h1>Day : ${day} <span>Hour : ${hour}</span></h1><p>Minutes : ${mints}</br> Seconds : ${sec}</p>`;
-if(day = 0 , hour = 0 ,mints = 0 , sec = 0){
+if(diff <= 0){
+    clearInterval(countDown);
     box.innerHTML = `<h1> EID MUBARAK </h1>`;
 
 }
-
+},1000);
