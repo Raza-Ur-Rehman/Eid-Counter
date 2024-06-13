b512c876c70b00c49cbe142ddd2710414321ff37
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-06-13 22:41:16 +0500

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ let hour = Math.floor((diff % ( 1000 * 60 * 60 * 24 )) / (1000 * 60 *60));
 let mints = Math.floor((diff % ( 1000 * 60 * 60 )) / (1000 * 60 )) ;
 let sec = Math.floor((diff % ( 1000 * 60)) / (1000));
 let box = document.querySelector ('.box');
-box.innerHTML = `<h1>Day : ${day} <span>Hour : ${hour}</span></h1><p>Minutes : ${mints}</br> Seconds : ${sec}</p>`;
+box.innerHTML = `<h1>Day : ${day} <span>Hour : ${hour}</span></h1><p>Minutes : ${mints}</br> </br>Seconds : ${sec}</p>`;
 if(diff <= 0){
     clearInterval(countDown);
     box.innerHTML = `<h1> EID MUBARAK </h1>`;
